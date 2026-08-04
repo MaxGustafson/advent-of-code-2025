cba2639775225fe5507df69bc3b49d375b679f62
Max Gustafson <maxgustafson97@gmail.com>
2026-08-04 18:32:18 +0200

    day_6: working solution part 2

--- solutions/day_6/data_reader.py
+++ solutions/day_6/data_reader.py
@@ -15,7 +15,55 @@ class DataReader():
                         return [r.strip() for r in file.read().strip().split('\n')]
 
         def _structure_input(self, input_rows: list[str]):
-
                 return [r.split() for r in input_rows]
 
+
+        def read_input_cephalopod(self):
+
+                print("***Entering Function***")
+                #Cephalopod_numbers in machine readable format
+                #Structure: [groups[numbers]]
+                cephalopod_numbers : list[int] = list()
+
+                #Operators
+                operators : list[str] = []
+
+                with open(self.input_path, 'r') as file:
+                        rows = file.read().split('\n')
+                        operators = rows.pop(-1).split()
+                        
+                        for row in rows:
+                                if not cephalopod_numbers:
+                                        cephalopod_numbers = [0 for i in row]
+
+                                for i in range(len(row)):
+                                        char = row[i]
+                                        print(f"Evaluating: {char}")
+
+                                        if char.isnumeric():
+                                                print(f"{cephalopod_numbers[i]} -> {cephalopod_numbers[i] * 10 + int(char)}")
+                                                #increase significance of existing numbers
+                                                cephalopod_numbers[i] =  cephalopod_numbers[i] * 10 + int(char)
+
+                #Group numbers into respective problem
+                cephalopod_numbers_grouped = list()
+                nbr_group = list()
+
+                #Only numbers which equals 0 have been produced by a column of blank spaces. Exactly what indicates a new group.
+                cephalopod_numbers.append(0) #Ensure last group is closed according to rule
+                for nbr in cephalopod_numbers:
+                        
+                        if nbr == 0:
+                                cephalopod_numbers_grouped.append(nbr_group)
+                                nbr_group = list() #Reset the group
+                        else:
+                                nbr_group.append(nbr)
+
+                #Sanity check
+                assert len(cephalopod_numbers_grouped) == len(operators)
+
+                return cephalopod_numbers_grouped, operators
+
+
+                                        
 
--- solutions/day_6/day_6.py
+++ solutions/day_6/day_6.py
@@ -4,7 +4,7 @@ import math
 
 _PATH : Path = "/".join(['day_6','data','input.txt'])
 
-def _solve_cephalopod_math_problems(input_rows: list[list[str]]):
+def _structure_cephalopod_math_problems_part_1(input_rows: list[list[str]]):
 
         #Get array of operations
         operations : list [str] = input_rows[-1]
@@ -20,15 +20,19 @@ def _solve_cephalopod_math_problems(input_rows: list[list[str]]):
                 for j in range(len(row)):
                     numbers[j].append(int(row[j]))
 
+        return numbers, operations
+
+def _solve_cephalopod_math_problems(number_groups : list[list[int]], operators : list[str]):
+
         #Solve problems
         ttl_sum : int = 0
 
-        for i in range(len(operations)):
-            if operations[i] == '+':
-                ttl_sum += sum(numbers[i])
+        for i in range(len(operators)):
+            if operators[i] == '+':
+                ttl_sum += sum(number_groups[i])
 
-            elif operations[i] == '*':
-                ttl_sum+= math.prod(numbers[i])
+            elif operators[i] == '*':
+                ttl_sum+= math.prod(number_groups[i])
 
             else:
                 raise(ValueError, "Unhandled operator!")
@@ -41,8 +45,14 @@ def main():
 
         input_rows : list[list[str]] = data_reader.read_input()
 
-        total_sum: int = _solve_cephalopod_math_problems(input_rows=input_rows)
-        print(f"The total sum of the maths problems are : {total_sum}")
+        numbers, operators = _structure_cephalopod_math_problems_part_1(input_rows=input_rows)
+        total_sum: int = _solve_cephalopod_math_problems(numbers, operators)
+        print(f"The total sum of the maths problems with part 1 structure is : {total_sum}")
+
+        number_groups, operators = data_reader.read_input_cephalopod()
+
+        total_sum: int = _solve_cephalopod_math_problems(number_groups, operators)
+        print(f"The total sum of the maths problems with Cephalopod structure is : {total_sum}")
 
 
 if __name__ == "__main__":
